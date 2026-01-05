0ecb8b6b213f95fc3633cb8c7cc56f7ba49bb1a6
Shironov <onlyworkshironov@gmail.com>
2026-01-05 22:22:57 +0800

    i homework

---
+++ untitled/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        //Год рожденияи и возраст
+
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год рождения: ");
+        int birthYear = scanner.nextInt();
+        int currentYear = 2026;
+        int age = currentYear - birthYear;
+        System.out.println("Ваш возраст " + age);*/
+
+
+
+        //Проценты
+
+        /*Scanner scanner = new Scanner(System.in);
+
+
+        while(true){
+            System.out.println("Введите сумму: ");
+            float sum = scanner.nextFloat();
+            System.out.println("Чаевые: ");
+            int tips = scanner.nextInt();
+            switch (tips){
+
+
+                case 10:
+                    System.out.println("Чаевые: " + sum * 0.1);
+                    break;
+                case 15:
+                    System.out.println("Чаевые: " + sum * 0.15);
+                        break;
+                case 30:
+                    System.out.println("Чаевые: " + sum * 0.3);
+                        break;
+                default:
+                    System.out.println("Некорректный ввод! Введите 10, 15 или 30: ");
+                    tips = scanner.nextInt();
+                    break;
+
+
+            }*/
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите температуру в градусах Цельсия: ");
+
+        double tempC = scanner.nextDouble();
+
+        double tempF = tempC * 1.8 + 32;
+
+        System.out.println("Температура в Фаренгейтах: " + tempF);
+
+
+
+    }
+}
